f72b5a5d6947d4213f64285d0482540dc28dd2cf
John Prada <jhonprada@gmail.com>
2019-09-09 10:41:25 -0500

    Auto stash before rebase of "origin/master"

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//Dropdown trigger
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, {
+    hover: true
+  });
+});
